196af8cb904f116e9fffd0a26f898156e51d7e1c
QuentinN42 <32516498+QuentinN42@users.noreply.github.com>
2023-03-28 11:26:49 -0700

    fix: docs baseurl (#14)
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Escape documentation',
   tagline: '',
-  url: 'https://escape.tech',
+  url: 'https://docs.escape.tech',
   baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
